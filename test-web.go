c896f6b507ff81229fb5f804b860f09f12ebf5c3
Scott Dixon <dixon.scottr@gmail.com>
2018-12-20 10:31:38 -0500

    Add test handler

--- test-web.go
+++ test-web.go
@@ -13,6 +13,12 @@ func getHeaderValue(r *http.Request, key string) string {
     return value
 }
 
+func printSpan(r *http.Request) {
+    if span, ok := tracer.SpanFromContext(r.Context()); ok {
+      fmt.Print(span)
+    }
+}
+
 func tagSpan(r *http.Request, key string, value string) {
     if span, ok := tracer.SpanFromContext(r.Context()); ok {
       fmt.Print(span)
@@ -20,7 +26,7 @@ func tagSpan(r *http.Request, key string, value string) {
     }
 }
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
       fmt.Printf("hello\n")
       fmt.Print(r.Header)
       user_agent := getHeaderValue(r, "User-Agent")
@@ -37,14 +43,23 @@ func GetWord(w http.ResponseWriter, r *http.Request) {
 func GetID(w http.ResponseWriter, r *http.Request) {
       vars := mux.Vars(r)
       w.WriteHeader(http.StatusOK)
+      printSpan(r)
       w.Write([]byte(fmt.Sprintf("ID: %v\n", vars["id"])))
+    }
+
+func Test(w http.ResponseWriter, r *http.Request) {
+      printSpan(r)
+      resp, err := http.Get("http://localhost:8081/test2")
+      fmt.Print(resp.Header)
+      w.Write([]byte(fmt.Sprintf("Body: %v\n", body)))
     }
 
 func main() {
       tracer.Start(tracer.WithDebugMode(true), tracer.WithPrioritySampling())
       mux := muxtrace.NewRouter()
-      mux.HandleFunc("/", YourHandler)
+      mux.HandleFunc("/", IndexHandler)
       mux.HandleFunc("/words/{word}", GetWord)
       mux.HandleFunc("/ids/{id}", GetID)
+      mux.HandleFunc("/test", Test)
       http.ListenAndServe(":8888", mux)
     }
